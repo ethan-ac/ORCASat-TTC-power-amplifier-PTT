ead9a74956102daec3bfb7bb6bce6fb260df4dd2
ethan-ac <ethanaaronclarke@gmail.com>
2021-04-08 15:29:27 -0700

    removed accidental build directory

--- gr-ampkey/build/docs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ethan/gr-ampkey/docs
-# Build directory: /home/ethan/gr-ampkey/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("doxygen")
--- gr-ampkey/build/include/ampkey/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ethan/gr-ampkey/include/ampkey
-# Build directory: /home/ethan/gr-ampkey/build/include/ampkey
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
